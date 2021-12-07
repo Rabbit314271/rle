93f16b4f978bd81d9b7211f7711cac9777276482
曾逸飞 <1207340135@qq.com>
2021-12-07 14:02:08 +0000

    update rlepose/datasets/custom.py.

--- rlepose/datasets/custom.py
+++ rlepose/datasets/custom.py
@@ -3,7 +3,7 @@ import copy
 import os
 import pickle as pk
 from abc import abstractmethod, abstractproperty
-
+import logging
 import cv2
 import torch.utils.data as data
 from rlepose.utils.presets import SimpleTransform
@@ -85,7 +85,7 @@ class CustomDataset(data.Dataset):
 
         # load ground truth, including bbox, keypoints, image size
         label = copy.deepcopy(self._labels[idx])
-
+        logging.debug(img_path)
         img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
 
         # transform ground truth into training label and apply data augmentation
